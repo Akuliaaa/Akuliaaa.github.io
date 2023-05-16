9861cbb4b97e0710b0ecd0589a5e66522c4fd149
Akuliaaa <132068279+Akuliaaa@users.noreply.github.com>
2023-05-17 00:35:46 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
-const closeKursi = document.getElementById("closeKursi");
-const openKursi = document.getElementById("katalogText1")
-const backList = document.getElementById("backList");
-const kursi = document.getElementById("kursi");
-const TextKursi = document.getElementById("TextKursi");
-const openPriceList = document.getElementById("openPriceList")
-const PriceList = document.getElementsByClassName("PriceList")
+const Login = document.getElementsByClassName('login')
 
+Login.addEventListener('click', function clickMouse() {
+    window.location.href = "login.html";
+});
 
-openKursi.addEventListener("click", ()=> {
-    kursi.style = "opacity: 1; visibility: visible;";
-    PriceList[0].style = "opacity: 0; visibility: hidden;";
-});
-backList.addEventListener("click", ()=> {
-    kursi.style = "opacity: 0; visibility: hidden;";
-    PriceList[0].style = "opacity: 0; visibility: hidden;";
-});
-kursi.addEventListener("mousedown", ()=> {
-    TextKursi.style = 'animation: left 0.2s esea 1; animation-fill-mode: for fowards;';
-});
-openPriceList.addEventListener("click", ()=> {
-    PriceList[0].style = "opacity: 1; visibility: visible;";
-    kursi.style = "opacity: 0; visibility: hidden;";
-});
